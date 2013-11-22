1065a4dc8805e06854d2816c2549641034baba79
Xonex <abrahamfontana@gmail.com>
2013-11-22 16:50:31 +0100

    Create datos.php

---
+++ datos.php
@@ -0,0 +1,13 @@
+<?php
+
+	/**     ACCESO  APPSB Y CONFIGURACION DE LA APP **/
+	$SBCode = ""; //codigo sb de la app
+	$SBKey = ""; //key de la app sb
+	
+	/** ACCESO  TWITTER **/
+	$settings = array( //Datos de acceso a la Api de Twitter
+			'oauth_access_token' => "",
+			'oauth_access_token_secret' => "",
+			'consumer_key' => "",
+			'consumer_secret' => ""
+	);
